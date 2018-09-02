0a85773250954fad04900c2bccf702adbf6ede0d
c00lske <david.wj.cools@gmail.com>
2018-09-02 17:06:43 +0200

    initial commit

---
+++ exc5.py
@@ -0,0 +1,18 @@
+my_name = 'David Cools'
+my_age = 49
+my_height = 1.82 #meter
+my_weight = 120 #kg
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'light brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} meter tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that is too much.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
